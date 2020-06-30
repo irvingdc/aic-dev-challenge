a354d19ebbbbb7b482a968b3359db791a7346509
irvingdc <irvingedc@gmail.com>
2020-06-30 14:43:51 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Comments from "./components/Comments/Comments.js";
+
+export default () => (
+  <div>
+    <p>All your components go here.</p>
+    <Comments />
+  </div>
+);
---
+++ src/components/Comments/Comments.js
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "./Comments.module.css"
+export default () => (
+  <div className={classes.container}>
+      Comments component.
+  </div>
+);
